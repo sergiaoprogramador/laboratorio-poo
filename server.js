447d04c35e69508690e4303f62075adaca9c5f68
Sergio Ramos <sergio.ramos@pgmais.com.br>
2021-05-25 20:14:01 -0400

    class 3 server http with node.js

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const http = require('http');
+// const dt = require('./time');
+const url = require('url');
+const fs = require('fs');
 
 http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/json'});
-    res.end("Hello world!")
-}).listen(8080)
+    fs.readFile("index.html", function(err, data){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    })
+    // let query = url.parse(req.url, true).query;
+    // res.end("Login: " + query.login + " - " + "Senha: " + query.senha);
+}).listen(8080);
