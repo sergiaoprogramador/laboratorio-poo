191d5e963ca889b7ce18dcee6f36eedea359623d
Sergio Ramos <sergio.ramos@pgmais.com.br>
2021-06-01 19:32:46 -0400

    json

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 const http = require('http');
-// const dt = require('./time');
 const url = require('url');
 const fs = require('fs');
 
 http.createServer(function(req, res) {
-    fs.readFile("index.html", function(err, data){
-        res.writeHead(200, {'Content-Type': 'text/html'});
+    fs.readFile("usuario.json", function(err, data){
+        res.writeHead(200, {'Content-Type': 'text/json'});
         res.write(data);
         return res.end();
     })
-    // let query = url.parse(req.url, true).query;
-    // res.end("Login: " + query.login + " - " + "Senha: " + query.senha);
 }).listen(8080);
