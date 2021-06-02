8cbd08140372eb05dd5a77877b296d6d5f016925
Sergio Ramos <sergio.ramos@pgmais.com.br>
2021-06-01 20:23:26 -0400

    file upload

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 const http = require('http');
-const url = require('url');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
 const fs = require('fs');
+const formidable = require('formidable');
 
 http.createServer(function(req, res) {
-    fs.readFile("usuario.json", function(err, data){
-        res.writeHead(200, {'Content-Type': 'text/json'});
-        res.write(data);
-        return res.end();
-    })
-}).listen(8080);
+    if (req.url == '/enviar/arquivo') {
+        const form = formidable({ multiples: true });
+
+        form.parse(req, (err, fields, files) => {
+            console.log(files.files, fields);
+            res.writeHead(200, {'Content-Type': 'text/json'});
+            res.end(JSON.stringify({ fields, files }, null, 2));
+        })
+
+        return;
+    } else {
+        fs.readFile("formFile.html", function(err, data) {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(data);
+            return res.end();
+        })
+    }
+}).listen(port, hostname, () => {
+    console.log(`Servidor rodando em http://${hostname}:${port}/`);
+})
